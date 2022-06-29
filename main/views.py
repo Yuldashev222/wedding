b9fad76fd2e0e4e7e9080e41efe46a4ca684966e
Oybek Yuldashev <oybekyuldashov54@gmail.com>
2022-06-29 17:31:08 +0500

    second commit

--- main/views.py
+++ main/views.py
@@ -3,3 +3,35 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html", {})
+
+
+def love_story(request):
+    return render(request, "love_story.html", {})
+
+
+def faq(request):
+    return render(request, "faq.html", {})
+
+
+def event(request):
+    return render(request, "event.html", {})
+
+
+def family(request):
+    return render(request, "family.html", {})
+
+
+def gallery(request):
+    return render(request, "gallery.html", {})
+
+
+def blog(request):
+    return render(request, "blog.html", {})
+
+
+def blog_single(request):
+    return render(request, "blog_single.html", {})
+
+
+def contact(request):
+    return render(request, "contact.html", {})
