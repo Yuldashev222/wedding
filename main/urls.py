3184cad9d0af76af78ec9dc9dfd1f5a018df4bdc
Oybek Yuldashev <oybekyuldashov54@gmail.com>
2022-07-01 08:36:48 +0500

    upload

--- main/urls.py
+++ main/urls.py
@@ -3,12 +3,4 @@ from .views import *
 
 urlpatterns = [
     path("", index, name="index"),
-    path("love_story/", love_story, name="love_story"),
-    path("faq/", faq, name="faq"),
-    path("event/", event, name="event"),
-    path("family/", family, name="family"),
-    path("gallery/", gallery, name="gallery"),
-    path("blog/", blog, name="blog"),
-    path("blog_single/", blog_single, name="blog_single"),
-    path("contact/", contact, name="contact"),
 ]
